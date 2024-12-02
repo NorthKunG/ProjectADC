c42f8e492efcda55054b90959adc0049189ea379
NorthKunG <nitipat1389@gmail.com>
2024-12-02 12:38:22 +0700

    Back-End Update an api new address on authController.js file

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,17 +1,22 @@
+// Require a jsonwebtoken
 const jwt = require('jsonwebtoken');
+
+// Require a user model
 const User = require('../models/userModel');
 
+// Login
 const login = async (req, res) => {
     try {
-        const user = await User.findOne(req.body.email);
-        if (!user || !(await user.matchPassword(req.body.password))) {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await user.matchPassword(password))) {
             res.status(400).json({
                 status: 'error',
                 message: 'Email or password is incorrect'
             });
             return;
         }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '6h' });
         res.status(200).json({
             status: 'success',
             token
@@ -26,22 +31,28 @@ const login = async (req, res) => {
     }
 };
 
+// Register
 const register = async (req, res) => {
     try {
-        const user = await User.findOne(req.body.email);
-        if (user) {
+        const { name, email, password, phoneNumber, address } = req.body;
+
+        // Check require fields
+        if (!name || !email || !password || !phoneNumber || !address) {
             res.status(400).json({
                 status: 'error',
-                message: 'Email is already'
+                message: 'All fields are required'
             });
             return;
         }
-        const createUser = await User.create(req.body);
-        res.status(200).json({
+
+        // Create a new user
+        const newUser = new User({ name, email, password, phoneNumber, address });
+        await newUser.save();
+
+        res.status(201).json({
             status: 'success',
-            message: 'Register successfully'
+            message: 'User registered successfully'
         });
-        return;
     } catch (error) {
         res.status(400).json({
             status: 'error',
@@ -51,7 +62,57 @@ const register = async (req, res) => {
     }
 };
 
+// Add new address
+const newAddress = async (req, res) => {
+    try {
+        const { newAddress } = req.body;
+
+        // Check a new address
+        if (!newAddress) {
+            res.status(400).json({
+                status: 'error',
+                message: 'New address is required'
+            });
+            return;
+        }
+
+        // Puu user data from token
+        const userId = req.userId;
+        console.log(userId);
+
+        // Update address in field address
+        const user = await User.findById(userId);
+        if (!user) {
+            res.status(404).json({
+                status: 'error',
+                message: 'User not found'
+            });
+            return;
+        }
+
+        // Add new address to array
+        user.address.push(newAddress);
+        await user.save();
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Address added successfully',
+            user: user.name,
+            address: user.address
+        });
+        return;
+    } catch (error) {
+        res.status(500).json({
+            status: 'error',
+            message: error.message
+        });
+        return;
+    }
+};
+
+// Export an api
 module.exports = {
     login,
-    register
-}//
+    register,
+    newAddress
+}
