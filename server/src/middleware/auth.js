51c73b1677906c0943d57b1d604e766a31778f4d
NorthKunG <nitipat1389@gmail.com>
2024-12-02 12:39:30 +0700

    Back-End Update auth.js file

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = decoded;
+        req.userId = decoded.id;
         next();
         return;
     } catch (error) {
