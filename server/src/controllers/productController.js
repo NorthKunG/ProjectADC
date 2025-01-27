a426a94ac40bd543e713b5213d237d1d259e9341
NorthKunG <nitipat1389@gmail.com>
2025-01-27 14:57:37 +0700

    Update

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -74,6 +74,7 @@ const getProducts = async (req, res) => {
                     name: 1,
                     // แสดงราคา
                     price: 1,
+                    ict: 1,
                     // แสดงชื่อหมวดหมู่ (ใช้ชื่อหมวดหมู่จาก `Category`)
                     category: '$categoryDetails.name',
                     // แสดง Features ของสินค้า
